49820e073e156a6fd3a477832b89aafa90bde605
James Turello <jturello@yahoo.com>
2016-07-08 12:06:49 -0700

    add code to pass unit test - Definition#initialize raise ArgumentError when input is empty string

--- lib/definition.rb
+++ lib/definition.rb
@@ -2,6 +2,11 @@ class Definition
   attr_reader :text
 
   define_method(:initialize) do |attributes|
+
+    if attributes[:text] == ""
+      raise ArgumentError.new('Invalid input: empty string not allowed!')
+    end
+    
     @text = attributes[:text]
   end
 
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -4,14 +4,23 @@ require('definition')
 describe(Definition) do
   describe('#initialize') do
 
-    it('instantiates a definition object') do
-      definition = Definition.new({:text => ""})
+    it('instantiate a definition object') do
+      definition = Definition.new({:text => "some text"})
       expect(definition.class).to eq(Definition)
     end
 
-    it('instantiates a definition object with the text provided') do
+    it('instantiate a definition object with the text provided') do
       definition = Definition.new({:text => 'This is the text provided to the new() method'})
-      expect(definition.text).to eq('This is the text provided to the new() method')
+      expect(definition.text()).to eq('This is the text provided to the new() method')
+    end
+
+    it("raise an ArgumentError when text parameter is an empty string") do
+      begin
+        definition = Definition.new({:text => ""})
+      rescue ArgumentError => e
+        expect(e.message).to eq('Invalid input: empty string not allowed!')
+      end
+      expect(definition).to eq(nil)
     end
 
   end

7085286f6e8bac9aa53ebf753989dd4b2552243b
James Turello <jturello@yahoo.com>
2016-07-13 01:13:36 -0700

    refactor code to make routes more RESTful

--- app.rb
+++ app.rb
@@ -11,21 +11,25 @@ get('/') do
   erb(:index)
 end
 
-post('/word_form') do
-  word = params[:word]
-  definition = params[:definition]
-  @word = Word.new({:text => word, :definition => definition})
-  @word.save()
-  erb(:word)
-end
-
 get('/word_form') do
   erb(:word_form)
 end
 
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i)
+  erb(:word)
+end
+
 post('/word') do
-  word_id = params[:word_id].to_i
-  @word = Word.find(word_id)
+  @word = Word.find(params[:word_id].to_i)
   @word.add_definition(params[:definition])
+  erb(:word)
+end
+
+post('/words/new') do
+  word = params[:word]
+  definition = params[:definition]
+  @word = Word.new({:text => word, :definition => definition})
+  @word.save()
   erb(:word)
 end
